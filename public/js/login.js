b68f51fe8dfd79290841706b30739b688d5938a1
andrewvu71 <andrewvu71@gmail.com>
2024-05-14 11:57:20 -0500

    Solved conflict in login.js

--- public/js/login.js
+++ public/js/login.js
@@ -15,7 +15,7 @@ const loginFormHandler = async (event) => {
     });
 
     if (response.ok) {
-      document.location.replace('/viewRecipes');
+      document.location.replace('/');
     } else {
       alert('Failed to log in');
     }
@@ -40,7 +40,7 @@ const signupFormHandler = async (event) => {
     });
 
     if (response.ok) {
-      document.location.replace('/viewRecipes');
+      document.location.replace('/');
     } else {
       alert('Failed to log in');
     }
